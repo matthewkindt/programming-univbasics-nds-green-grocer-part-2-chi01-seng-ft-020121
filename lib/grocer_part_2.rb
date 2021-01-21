96061c6d14a27cbbd3ea934e82108cbab6bb4fb4
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-21 02:28:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -26,8 +26,13 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+cart.map do |item|
+if item[;clearance]
+item[;price] *= 0.8
 end
-
+item
+end
+end
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
   #
